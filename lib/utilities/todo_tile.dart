8d0dd052de5b6e7790f6b1d36bc93f281bcfce6f
denisesarona <135094209+denisesarona@users.noreply.github.com>
2023-11-26 10:28:02 +0800

    Create a todotile (util page for list of tasks)

---
+++ lib/utilities/todo_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoTile extends StatelessWidget {
+  const TodoTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
