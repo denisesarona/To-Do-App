ce312bfd5bccde89cfeb7f9288c30e9e3a51d9a6
denisesarona <135094209+denisesarona@users.noreply.github.com>
2023-11-26 10:16:41 +0800

    Added a theme for the app

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomePage(), 
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
     );
   }
 }
