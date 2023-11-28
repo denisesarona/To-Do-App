7391a3bb667a71cc686525ff2711b5dd41f5ddc3
denisesarona <135094209+denisesarona@users.noreply.github.com>
2023-11-29 07:04:43 +0800

    Updated the padding from edge to only

--- lib/utilities/todo_tile.dart
+++ lib/utilities/todo_tile.dart
@@ -15,7 +15,7 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(25.0),
+      padding: EdgeInsets.only(left:25.0, right:25, top:25),
       child: Container(
         padding: EdgeInsets.all(25),
         child: Row(
