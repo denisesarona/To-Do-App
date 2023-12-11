4b02fb4fe64f76be237062f39ae2535600e2273f
denisesarona <135094209+denisesarona@users.noreply.github.com>
2023-12-11 23:17:32 +0800

    made the clock form the date package, and customized it

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,45 +1,112 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-import '../utilities/todo_tile.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
+class DigitalClock extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  _DigitalClockState createState() => _DigitalClockState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _DigitalClockState extends State<DigitalClock> {
+  late DateTime _dateTime; // Use 'late' to indicate late initialization
+  late Timer _timer;
 
-  // List of to do tasks
-  List todoList = [
-    ["Make tutorial", false], ["Do exercis", false], 
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _dateTime = DateTime.now();
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+  }
 
-  // if checkbox was tapped
-  void checkboxChanged (bool? value, int index) {
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _getTime() {
     setState(() {
-      todoList[index][1] = !todoList[index][1];
+      _dateTime = DateTime.now();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple[300],
-        centerTitle: true,
-        title: Text('TO DO'),
-        elevation: 0.0,
-      ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder:(context, index) {
-          return TodoTile(
-            taskName: todoList[index][0], 
-            taskCompleted: todoList[index][1], 
-            onChanged:(value) => checkboxChanged(value, index));
-        },
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(30)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 70.0, bottom: 70),
+                      child: Text(
+                        DateFormat('HH').format(_dateTime), // Show only hours
+                        style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.white, fontSize: 150),
+
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Container(
+                decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(30)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start, // Align to the left
+                  crossAxisAlignment: CrossAxisAlignment.start, // Align day of the week to the left
+                  children: <Widget>[ // Add space between day and minutes
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 70.0, bottom: 70),
+                        child: Text(
+                          DateFormat('mm').format(_dateTime), // Show only minutes
+                          style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.white, fontSize: 150),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(
+                              DateFormat('EEEE').format(_dateTime), // Show the first day of the week
+                              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Text(
+                              DateFormat('d').format(_dateTime), // Show the second day of the week
+                              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
