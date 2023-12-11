8c4a16cb3fc03911b7e9ed47c2f224189e62df8c
denisesarona <135094209+denisesarona@users.noreply.github.com>
2023-12-11 23:16:39 +0800

    Added a home digital clock

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/pages/home_page.dart';
+import 'package:to_do_app/pages/home_page.dart'; // Add this import for DateFormat
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(), 
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: 'Poppins',
-      ),
+      home: DigitalClock(),
     );
   }
 }
