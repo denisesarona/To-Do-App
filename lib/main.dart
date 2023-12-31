1ae047b02b6d7f50ceea4a9d08b2e2bdc2447c31
denisesarona <135094209+denisesarona@users.noreply.github.com>
2023-12-31 11:43:23 +0800

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/pages/home_page.dart';
-import 'package:intl/intl.dart';
  // Add this import for DateFormat
 
 void main() {
-  Intl.defaultLocale = 'fil_PH';
   runApp(const MyApp());
 }
 
